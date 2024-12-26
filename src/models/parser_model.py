fc104afa9e932b20893425273fafd50b833d29be
artur-titov <fop.titov.av@gmail.com>
2024-12-26 23:58:45 +0100

    add docstring

--- src/models/parser_model.py
+++ src/models/parser_model.py
@@ -1,15 +1,25 @@
-"""Module description"""
+"""Provides file parsing functionality."""
 
 from pathlib import Path
 import yaml
 
 
 class ParserModel:
-    """Class description"""
+    """Provides methods for file parsing."""
 
     def yml(self, filepath: Path) -> dict:
-        """Method description"""
+        """Translates a YAML file into a Python dictionary.
+        
+        Parameters
+        ----------
+        filepath : Path
+            Path to file.
 
+        Returns
+        -------
+        config : dict
+            Configuration file as a Python dictionary.
+        """
         with open(filepath, encoding="utf-8") as configuration:
             config = yaml.load(configuration, Loader=yaml.SafeLoader)
 
